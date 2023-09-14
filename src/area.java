4515630d7ffd1ed223333aab1c610c93ecf799c6
Rodney <rodneyandrade1@hotmail.com>
2023-09-13 23:17:16 -0300

    Initial commit

---
+++ src/area.java
@@ -0,0 +1,9 @@
+public class area {
+    public static void main(String[] args) {
+        System.out.println("Exercício de Quadrilátero notáveis");
+        Quadrilatero.area(5);
+        Quadrilatero.area(5d,5d);
+        Quadrilatero.area(7,8,9);
+        Quadrilatero.area(5f,5f);
+    }
+}
